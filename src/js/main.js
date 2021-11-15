300ec9245a41f743dbc95b7ac0c502952e88ff25
VinsFlo <vinsflorineus@gmail.com>
2021-11-15 18:09:01 +0300

    feat: Fetch API

--- src/js/main.js
+++ src/js/main.js
@@ -238,33 +238,33 @@ window.addEventListener('DOMContentLoaded', function () {
 
 			let statusMessage = document.createElement('img');
 			statusMessage.src = message.loading;
-			statusMessage.classList.add('status');
-			statusMessage.textContent = message.loading;
-			// form.appendChild(statusMessage);
+			statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
 			form.insertAdjacentElement('afterend', statusMessage);
 
-			const request = new XMLHttpRequest();
-			request.open('POST', 'server.php');
-			request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 			const formData = new FormData(form);
 
 			const object = {};
 			formData.forEach(function (value, key) {
 				object[key] = value;
 			});
-			const json = JSON.stringify(object);
 
-			request.send(json);
-
-			request.addEventListener('load', () => {
-				if (request.status === 200) {
-					console.log(request.response);
-					showThanksModal(message.success);
-					form.reset();
-					statusMessage.remove();
-				} else {
-					showThanksModal(message.failure);
-				}
+			fetch('server.php', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(object)
+			}).then(data => {
+				console.log(data);
+				showThanksModal(message.success);
+				statusMessage.remove();
+			}).catch(() => {
+				showThanksModal(message.failure);
+			}).finally(() => {
+				form.reset();
 			});
 		});
 	}
@@ -278,12 +278,11 @@ window.addEventListener('DOMContentLoaded', function () {
 		const thanksModal = document.createElement('div');
 		thanksModal.classList.add('modal__dialog');
 		thanksModal.innerHTML = `
-			<div class="modal__content">
-				<div class="modal__close" data-close>&times;</div>
-				<div class="modal__title">${message}</div>
-			</div>
-			`;
-
+            <div class="modal__content">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
 		document.querySelector('.modal').append(thanksModal);
 		setTimeout(() => {
 			thanksModal.remove();
